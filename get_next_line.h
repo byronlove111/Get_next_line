8c4a641fcc3d26a23e75c2de17ac76113fa3096d
Abd'Al-Malik Bourassi <abbouras@bess-f4r2s6.clusters.42paris.fr>
2024-11-25 13:11:32 +0100

    first init

---
+++ get_next_line.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abbouras <abbouras@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/25 11:55:21 by abbouras          #+#    #+#             */
+/*   Updated: 2024/11/25 12:02:17 by abbouras         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <stdlib.h>
+# include <unistd.h>
+
+char	*get_next_line(int fd);
+
+#endif
