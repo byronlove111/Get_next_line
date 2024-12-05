d4612e9fdadbe5b6faa316e5dabc27eef72fa202
Malik <malik@MacBook-Air-de-Alexis.local>
2024-12-05 03:43:28 +0100

    some steps

--- get_next_line.c
+++ get_next_line.c
@@ -10,16 +10,87 @@
 /*                                                                            */
 /* ************************************************************************** */
 #include "get_next_line.h"
+#include <stdio.h>
 
-static char	*get_next_line(int fd)
+static char	*fill_line_buffer(int fd, char *left_c, char *buffer);
+static char	*set_line(char *line_buffer);
+static void	*ft_calloc(size_t elementCount, size_t elementSize);
+
+char	*get_next_line(int fd)
+{
+	static char		*left_c = NULL;
+	char			*buffer;
+	char			*line;
+
+	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
+		return (NULL);
+	buffer = ft_calloc(BUFFER_SIZE + 1, sizeof(char));
+	if (!buffer)
+		return (NULL);
+	left_c = fill_line_buffer(fd, left_c, buffer);
+	free(buffer);
+	if (!left_c)
+		return (NULL);
+	line = set_line(left_c);
+	return (line);
+}
+
+static char	*fill_line_buffer(int fd, char *left_c, char *buffer)
+{
+	ssize_t		bytes_read;
+	char		*tmp;
+
+	bytes_read = 1;
+	while (bytes_read > 0)
+	{
+		bytes_read = read(fd, buffer, BUFFER_SIZE);
+		if (bytes_read == -1)
+		{
+			free(left_c);
+			return (NULL);
+		}
+		else if (bytes_read == 0)
+			break;
+		buffer[bytes_read] = 0;
+		if (!left_c)
+			left_c = ft_strdup("");
+		tmp = left_c;
+		left_c = ft_strjoin(tmp, buffer);
+		free(tmp);
+		tmp = NULL;
+		if (ft_strchr(buffer, '\n'))
+			break;
+	}
+	return (left_c);
+}
+
+static char	*set_line(char *line_buffer)
 {
 }
 
-static int	find_size(int nb)
+static void	*ft_calloc(size_t elementCount, size_t elementSize)
 {
+	unsigned char	*ptr;
+	size_t			i;
+
+	if (elementSize * elementCount > SIZE_MAX)
+		return (NULL);
+	ptr = malloc(elementSize * elementCount);
+	if (!ptr)
+		return (NULL);
+	i = 0;
+	while (i < (elementCount * elementSize))
+	{
+		ptr[i] = 0;
+		i++;
+	}
+	return (ptr);
 }
 
-// #include <stdio.h>
-// int	main(int ac, char **av)
-// {
-// }
+/*int	main(int ac, char **av)
+{
+	int fd = open("file.txt", O_RDONLY);
+	if (fd == -1)
+		return (NULL);
+	
+}*/
