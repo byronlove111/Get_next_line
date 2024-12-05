fdb0c3468e5da3c4e2fe67378f5f47960c49d186
Malik <malik@MacBook-Air-de-Alexis.local>
2024-12-05 06:23:54 +0100

    moty

--- get_next_line.c
+++ get_next_line.c
@@ -12,104 +12,27 @@
 #include "get_next_line.h"
 #include <stdio.h>
 
-static char	*fill_line_buffer(int fd, char *left_c, char *buffer);
-static char	*set_line(char *line_buffer);
-static void	*ft_calloc(size_t elementCount, size_t elementSize);
-
 char	*get_next_line(int fd)
 {
-	static char		*left_c = NULL;
+	static char		*stash = NULL;
 	char			*buffer;
 	char			*line;
+	int				bytes_read;
 
 	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
 		return (NULL);
-	buffer = ft_calloc(BUFFER_SIZE + 1, sizeof(char));
+	buffer = malloc(sizeof(char) * (BUFFER_SIZE + 1));
 	if (!buffer)
 		return (NULL);
-	left_c = fill_line_buffer(fd, left_c, buffer);
-	free(buffer);
-	if (!left_c)
-		return (NULL);
-	line = set_line(left_c);
-	return (line);
-}
-
-static char	*fill_line_buffer(int fd, char *left_c, char *buffer)
-{
-	ssize_t		bytes_read;
-	char		*tmp;
-
-	bytes_read = 1;
-	while (bytes_read > 0)
+	bytes_read = read(fd, buffer, BUFFER_SIZE);
+	if (bytes_read <= 0)
 	{
-		bytes_read = read(fd, buffer, BUFFER_SIZE);
-		if (bytes_read == -1)
-		{
-			free(left_c);
-			return (NULL);
-		}
-		else if (bytes_read == 0)
-			break;
-		buffer[bytes_read] = 0;
-		if (!left_c)
-			left_c = ft_strdup("");
-		tmp = left_c;
-		left_c = ft_strjoin(tmp, buffer);
-		free(tmp);
-		tmp = NULL;
-		if (ft_strchr(buffer, '\n'))
-			break;
+		free(buffer);
+		return (NULL);
 	}
-	return (left_c);
+	buffer[bytes_read] = '\0';
+	return (buffer);
 }
-
-static char	*set_line(char *line_buffer)
-{
-	char	*line;
-	char	*remainder;
-	ssize_t	i;
-
-	i = 0;
-	if (!line_buffer[0])
-		return (NULL);
-	while (line_buffer[i] != '\n' && line_buffer[i] != '\0')
-		i++;
-	line = ft_substr(line_buffer, 0, i + 1);
-	if (!line)
-		return (NULL);
-	if (line_buffer[i] == '\n')
-	{
-		remainder = ft_strdup(line_buffer + i + 1);
-		free(line_buffer);
-		line_buffer = remainder;
-	}
-	else
-		remainder = NULL;
-	free(line_buffer);
-	line_buffer = remainder;
-	return (line);
-}
-
-static void	*ft_calloc(size_t elementCount, size_t elementSize)
-{
-	unsigned char	*ptr;
-	size_t			i;
-
-	if (elementSize * elementCount > SIZE_MAX)
-		return (NULL);
-	ptr = malloc(elementSize * elementCount);
-	if (!ptr)
-		return (NULL);
-	i = 0;
-	while (i < (elementCount * elementSize))
-	{
-		ptr[i] = 0;
-		i++;
-	}
-	return (ptr);
-}
-
 int main(void)
 {
     int     fd;
@@ -122,11 +45,13 @@ int main(void)
         return (1);
     }
 
-    while ((line = get_next_line(fd)) != NULL)
-    {
-        printf("%s", line);
-        free(line);
-    }
+	char *str = NULL;
+	while ((str = get_next_line(fd)) != NULL)
+	{
+		printf("%s\n", str);
+		free(str);
+	}
+
     close(fd);
     return (0);
 }
