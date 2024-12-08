7e050fea0feebd03cb6443609dd7f0d04e0216af
Abd'al-Malik Bourassi <malik.chan94420@gmail.com>
2024-12-08 20:37:01 +0100

    norm

--- get_next_line.c
+++ get_next_line.c
@@ -15,8 +15,8 @@
 
 static char	*read_line(int fd, char *buffer, char *stash);
 static char	*clean_line(char *line, char **stash);
-void	*ft_calloc(size_t elementCount, size_t elementSize);
-static char *read_buffer(int fd, char *buffer, char *line);
+static char	*read_buffer(int fd, char *buffer, char *line);
+void		*ft_calloc(size_t elementCount, size_t elementSize);
 
 char	*get_next_line(int fd)
 {
@@ -47,51 +47,51 @@ char	*get_next_line(int fd)
 	return (sub_line);
 }
 
-static char *read_buffer(int fd, char *buffer, char *line)
+static char	*read_buffer(int fd, char *buffer, char *line)
 {
-    int     bytes_read;
-    char    *tmp;
+	char	*tmp;
+	int		bytes_read;
 
-    bytes_read = read(fd, buffer, BUFFER_SIZE);
-    if (bytes_read <= 0)
-    {
-        free(line);
-        return (NULL);
-    }
-    buffer[bytes_read] = '\0';
-    tmp = line;
-    line = ft_strjoin(line, buffer);
-    free(tmp);
-    return (line);
+	bytes_read = read(fd, buffer, BUFFER_SIZE);
+	if (bytes_read <= 0)
+	{
+		free(line);
+		return (NULL);
+	}
+	buffer[bytes_read] = '\0';
+	tmp = line;
+	line = ft_strjoin(line, buffer);
+	free(tmp);
+	return (line);
 }
 
-static char *read_line(int fd, char *buffer, char *stash)
+static char	*read_line(int fd, char *buffer, char *stash)
 {
-    char    *line;
-    char    *tmp;
+	char	*line;
+	char	*tmp;
 
-    line = ft_strdup("");
-    if (stash)
-    {
-        tmp = line;
-        line = ft_strjoin(stash, line);
-        free(tmp);
-    }
-    while (!ft_strchr(buffer, '\n'))
-    {
-        line = read_buffer(fd, buffer, line);
-        if (!line)
-            return (NULL);
-    }
-    free(buffer);
-    return (line);
+	line = ft_strdup("");
+	if (stash)
+	{
+		tmp = line;
+		line = ft_strjoin(stash, line);
+		free(tmp);
+	}
+	while (!ft_strchr(buffer, '\n'))
+	{
+		line = read_buffer(fd, buffer, line);
+		if (!line)
+			return (NULL);
+	}
+	free(buffer);
+	return (line);
 }
 
 static char	*clean_line(char *line, char **stash)
 {
-	size_t	i;
 	char		*new_line;
 	char		*new_stash;
+	size_t		i;
 
 	i = 0;
 	while (line[i] != '\n' && line[i] != '\0')
@@ -104,6 +104,7 @@ static char	*clean_line(char *line, char **stash)
 	*stash = new_stash;
 	return (new_line);
 }
+
 void	*ft_calloc(size_t elementCount, size_t elementSize)
 {
 	unsigned char	*ptr;
@@ -123,23 +124,23 @@ void	*ft_calloc(size_t elementCount, size_t elementSize)
 	return (ptr);
 }
 
-int main(void)
-{
-	int     fd;
-	char    *line;
+// int main(void)
+// {
+// 	int     fd;
+// 	char    *line;
 
-	fd = open("file.txt", O_RDONLY);
-	if (fd == -1)
-	{
-	  printf("Erreur d'ouverture du fichier\n");
-	  return (1);
-	}
-	while ((line = get_next_line(fd)) != NULL)
-	{
-		printf("%s\n", line);
-		free(line);
-	}
+// 	fd = open("file.txt", O_RDONLY);
+// 	if (fd == -1)
+// 	{
+// 	  printf("Erreur d'ouverture du fichier\n");
+// 	  return (1);
+// 	}
+// 	while ((line = get_next_line(fd)) != NULL)
+// 	{
+// 		printf("%s\n", line);
+// 		free(line);
+// 	}
 
-	close(fd);
-	return (0);
-}
+// 	close(fd);
+// 	return (0);
+// }
